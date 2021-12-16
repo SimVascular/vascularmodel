5deb4850cbc41f12fa60f12a28c6efa1c8df5e85
Luca Pegolotti <luca@MacBook-Pro.local>
2021-12-16 14:59:21 -0800

    Update tracking name

--- js/download_tracking.js
+++ js/download_tracking.js
@@ -1,7 +1,7 @@
  $("a").click( function() {
   var id = $(this).attr('id');
   console.log(id);
-  gtag('event', id, {
+  gtag('event', 'download_' + id.toString(), {
       'send_to': 'G-SRE0G5Y7Y5',
       'event_category': 'Model download',
       'event_label': 'test',
