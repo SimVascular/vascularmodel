996b79d43e4413576f94dccdd84f7699f4e95d0f
Luca Pegolotti <luca@MacBook-Pro.local>
2021-12-17 12:15:30 -0800

    Add FAQs

--- js/theme.js
+++ js/theme.js
@@ -12,44 +12,44 @@ $('.page-scroll').bind('click', function(event) {
 
 
 
-////////////////////////////////////////////////////////////////////////
-// On-Scroll Animated Header: https://github.com/codrops/AnimatedHeader
-////////////////////////////////////////////////////////////////////////
-
-var cbpAnimatedHeader = (function() {
-
-    var docElem = document.documentElement,
-        header = document.querySelector( '.navbar-fixed-top' ),
-        didScroll = false,
-        changeHeaderOn = 10;
-
-    function init() {
-        window.addEventListener( 'scroll', function( event ) {
-            if( !didScroll ) {
-                didScroll = true;
-                setTimeout( scrollPage, 250 );
-            }
-        }, false );
-    }
-
-    function scrollPage() {
-        var sy = scrollY();
-        if ( sy >= changeHeaderOn ) {
-            classie.add( header, 'navbar-shrink' );
-        }
-        else {
-            classie.remove( header, 'navbar-shrink' );
-        }
-        didScroll = false;
-    }
-
-    function scrollY() {
-        return window.pageYOffset || docElem.scrollTop;
-    }
-
-    init();
-
-})();
+// ////////////////////////////////////////////////////////////////////////
+// // On-Scroll Animated Header: https://github.com/codrops/AnimatedHeader
+// ////////////////////////////////////////////////////////////////////////
+//
+// var cbpAnimatedHeader = (function() {
+//
+//     var docElem = document.documentElement,
+//         header = document.querySelector( '.navbar-fixed-top' ),
+//         didScroll = false,
+//         changeHeaderOn = 10;
+//
+//     function init() {
+//         window.addEventListener( 'scroll', function( event ) {
+//             if( !didScroll ) {
+//                 didScroll = true;
+//                 setTimeout( scrollPage, 250 );
+//             }
+//         }, false );
+//     }
+//
+//     function scrollPage() {
+//         var sy = scrollY();
+//         if ( sy >= changeHeaderOn ) {
+//             classie.add( header, 'navbar-shrink' );
+//         }
+//         else {
+//             classie.remove( header, 'navbar-shrink' );
+//         }
+//         didScroll = false;
+//     }
+//
+//     function scrollY() {
+//         return window.pageYOffset || docElem.scrollTop;
+//     }
+//
+//     init();
+//
+// })();
 
 
 
