a1e8644989659800326e3134c393eafa6917b6b1
Luca Pegolotti <luca@MacBook-Pro-2.Home>
2021-12-24 10:19:25 +0100

    Update google analytics data to use simvascular profile

--- js/download_tracking.js
+++ js/download_tracking.js
@@ -6,7 +6,7 @@
     window.open('svprojects/' + id.toString() + '.zip')
     console.log('svprojects/' + id.toString() + '.zip')
     gtag('event', 'download_' + id.toString(), {
-        'send_to': 'G-SRE0G5Y7Y5',
+        'send_to': 'G-YVVR1546XJ',
         'event_category': 'Model download',
         'event_label': 'test',
         'value': '1'
