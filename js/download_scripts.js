56f0352793b2fb8a9f8769fb99a4bb5b41cbb2d5
Luca Pegolotti <luca@MacBook-Pro-2.Home>
2021-12-28 16:43:34 +0100

    Minor correction

--- js/download_scripts.js
+++ js/download_scripts.js
@@ -1,15 +1,17 @@
 let pathRoot = '/var/www/vascularmodel'
 
 function loadFile(filePath) {
-  var result = null;
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.open("GET", filePath, true);
-  xmlhttp.send();
-  if (xmlhttp.status==200) {
-    result = xmlhttp.responseText;
+  var result = null
+  var xmlhttp = new XMLHttpRequest()
+  xmlhttp.open("GET", filePath, true)
+  xmlhttp.send()
+  if (xmlhttp.status == 200) {
+    result = xmlhttp.responseText
+    console.log(result)
   }
+  console.log(result)
   // return result;
-  return xmlhttp;
+  return xmlhttp
 }
 
 let selectedModels = []
