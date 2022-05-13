679be354b2d01de6e55d8e1f3a4951a64573f561
lucapegolotti <lpego@stanford.edu>
2022-05-13 11:13:37 -0700

    Reactivate filters

--- js/main.js
+++ js/main.js
@@ -162,44 +162,44 @@ $(window).load(function(){
         }
       }
   });
-  //
-  // //search filtering
-  // //credits http://codepen.io/edprats/pen/pzAdg
-  // var inputText;
-  // var $matching = $();
-  //
-  // var delay = (function(){
-  //   var timer = 0;
-  //   return function(callback, ms){
-  //     clearTimeout (timer);
-  //       timer = setTimeout(callback, ms);
-  //   };
-  // })();
-  //
-  // $(".cd-filter-content input[type='search']").keyup(function(){
-  //     // Delay function invoked to make sure user stopped typing
-  //     delay(function(){
-  //       inputText = $(".cd-filter-content input[type='search']").val().toLowerCase();
-  //        // Check to see if input field is empty
-  //       if ((inputText.length) > 0) {
-  //           $('.mix').each(function() {
-  //             var $this = $(this);
-  //
-  //             // add item to be filtered out if input text matches items inside the title
-  //             if($this.attr('class').toLowerCase().match(inputText)) {
-  //                 $matching = $matching.add(this);
-  //             } else {
-  //                 // removes any previously matched item
-  //                 $matching = $matching.not(this);
-  //             }
-  //           });
-  //           $('.cd-gallery ul').mixItUp('filter', $matching);
-  //       } else {
-  //           // resets the filter to show all item if input is empty
-  //           $('.cd-gallery ul').mixItUp('filter', 'all');
-  //       }
-  //     }, 200 );
-  // });
+
+  //search filtering
+  //credits http://codepen.io/edprats/pen/pzAdg
+  var inputText;
+  var $matching = $();
+
+  var delay = (function(){
+    var timer = 0;
+    return function(callback, ms){
+      clearTimeout (timer);
+        timer = setTimeout(callback, ms);
+    };
+  })();
+
+  $(".cd-filter-content input[type='search']").keyup(function(){
+      // Delay function invoked to make sure user stopped typing
+      delay(function(){
+        inputText = $(".cd-filter-content input[type='search']").val().toLowerCase();
+         // Check to see if input field is empty
+        if ((inputText.length) > 0) {
+            $('.mix').each(function() {
+              var $this = $(this);
+
+              // add item to be filtered out if input text matches items inside the title
+              if($this.attr('class').toLowerCase().match(inputText)) {
+                  $matching = $matching.add(this);
+              } else {
+                  // removes any previously matched item
+                  $matching = $matching.not(this);
+              }
+            });
+            $('.cd-gallery ul').mixItUp('filter', $matching);
+        } else {
+            // resets the filter to show all item if input is empty
+            $('.cd-gallery ul').mixItUp('filter', 'all');
+        }
+      }, 200 );
+  });
 });
 
 /*****************************************************
