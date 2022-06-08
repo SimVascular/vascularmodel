5b29863a43f6cab7666c0618f76173aa7b79defe
Carole <carole.darve@gmail.com>
2022-06-08 12:30:04 -0700

    files organized! website seems to be working fine

--- js/globalVar.js
+++ js/globalVar.js
@@ -5,3 +5,65 @@ var curIndex = 0;
 var smallScreen = false
 var lastFapplied = 0;
 var lastFdata = [];
+
+function getAllCategories()
+{
+  var allCategories = []
+
+  for (const [key, value] of Object.entries(data[0])) {
+    allCategories.push(key);
+  }
+
+  return allCategories;
+}
+
+function getFilterTitles()
+{
+  var allCategories = getAllCategories()
+  var onlyFilterTitles = []
+
+  for(var i = 0; i < allCategories.length; i++)
+  {
+    if(allCategories[i] != "Size" && allCategories[i] != "Name")
+    {
+      onlyFilterTitles.push(allCategories[i])
+    }
+  }
+
+  return onlyFilterTitles;
+}
+
+function getCategoryName()
+{
+  var allCategories = getAllCategories()
+  var onlyTheAttributes = []
+
+  //skips Name
+  //ends before start of MustContain filters
+  for (var i = 1; i < allCategories.indexOf("Images"); i++)
+  {
+    onlyTheAttributes.push(allCategories[i]);
+  }
+
+  return onlyTheAttributes;
+}
+
+function getCheckboxName()
+{
+  var allCategories = []
+  var notDropDownNames = []
+
+  for (const [key, value] of Object.entries(data[0])) {
+    allCategories.push(key);
+  }
+
+  for (var i = 0; i < allCategories.length; i++)
+  {
+    if (getNTimesPerCategory(allCategories[i]) != 2 && allCategories[i] != "Name" && allCategories[i] != "Size")
+    {
+      notDropDownNames.push(allCategories[i]);
+    }
+  }
+
+  return notDropDownNames;
+}
