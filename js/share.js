94b3728658b83e3849b2ed65478fd3351807e18a
Carole <carole.darve@gmail.com>
2022-06-28 08:43:12 -0700

    comments share.js

--- js/share.js
+++ js/share.js
@@ -1,3 +1,4 @@
+//two global variables of share.js
 var models = []
 var model;
 
@@ -19,13 +20,16 @@ $(document).ready(function($){
 
 function getVariable()
 {
+    //reads what is after the ?
     const queryString = window.location.search;
     //skips ? and starts at what is after the ?
     var codedName = queryString.substring(1);
 
+    //decodes the info from the URL
     var encodedNames = decodeRLE(encodeATOB(codedName));
     var found = false;
 
+    //searches for which are selected
     for(var i = 0; i < encodedNames.length; i++)
     {
         if(encodedNames[i] == "Y")
@@ -37,20 +41,24 @@ function getVariable()
 
     if(!found)
     {
+        //error message if no model
         displayErrorMessage(1);
     }
     else if (models.length == 1)
     {
+        //display for one model
         displayErrorMessage(2);
         model = models[0];
         displayModel(model);
     }
     else{
+        // multiple model display
         displayErrorMessage(3);
         displayTableModels(models);
     }
 }
 
+//deals with which page to show
 function displayErrorMessage(num) {
     var errorMsg = document.getElementById("errorBlock");
     errorMsg.textContent = "It looks like there are no models to exhibit!";
@@ -60,24 +68,28 @@ function displayErrorMessage(num) {
 
     if(num == 1)
     {
+        //shows error message
         showDiv(errorMsg);
         hideDiv(whenModelSelected);
         hideDiv(whenModelsSelected);
     }
     else if (num == 2)
     {
+        //shows one model
         hideDiv(errorMsg);
         showDiv(whenModelSelected);
         hideDiv(whenModelsSelected);
     }
     else if (num == 3)
     {
+        //shows a table of multiple models
         hideDiv(errorMsg);
         hideDiv(whenModelSelected);
         showDiv(whenModelsSelected);
     }
 }
 
+//deals with hiding and showing DIVs
 function showDiv(div)
 {
     div.classList.remove("hide");
@@ -90,25 +102,31 @@ function hideDiv(div)
     div.classList.remove("show");
 }
 
+//displaying one model
 function displayModel()
 {
+    //creates "header"
     var div = document.getElementById("displayedModel");
     var title = document.createElement("h1");
     title.textContent = "You are viewing " + model["Name"] + ".";
 
+    //creates image
     let img = document.createElement("img");
     img.src = 'img/vmr-images/' + model['Name'] + '.png'
     img.alt = model['Name'];
     img.classList.add("imgContainer");
     img.classList.add("center");
 
+    //table of information on model
     var desc = getDescription(model);
 
+    //appends all to div
     div.appendChild(title);
     div.appendChild(img);
     div.appendChild(desc);
 }
 
+//deals with table of information
 function getDescription()
 {
     var table = document.createElement("table");
@@ -116,38 +134,46 @@ function getDescription()
 
     for(var d = 0; d < categoryName.length; d++)
     {
+        //new row for each detail
         var newTR = document.createElement("tr");
 
+        //takes in title of detail
         var newHeader = document.createElement("th");
         newHeader.textContent = categoryName[d];
         
+        //newColumn is the information on the model
         var newColumn = document.createElement("td");
         var details = "";
         
         var valInCat = model[categoryName[d]];
         
+        //if no value is specified in the CSVs
         if(valInCat == "-")
         {
             details += "N/A";
         }
-        
+        //deals differently with each information
         else{
             if(categoryName[d] == "Age")
             {
+                //gives back most relevant unit
                 details += ageCalculator(valInCat);
             }
             else if(categoryName[d] == "Species" && valInCat == "Animal")
             {
+                //more specific species
                 details += model["Animal"];
             }
             else if(categoryName[d] == "Notes")
             {
+                //works with URLs and \n for notes
                 if(model["Notes"] != '-')
                 {
                     notes = model["Notes"];
                     if(notes.includes("\\url"))
                     {
                         var string = notes;
+                        //allows for multiple URLs
                         while(string.includes("\\url"))
                         {
                             var output = URLMaker(string);
@@ -163,20 +189,24 @@ function getDescription()
             }
             else if(categoryName[d] == "Size")
             {
+                //deals with units for size
                 var size = parseInt(model['Size']) / 1000000
                 details += size.toFixed(2) + ' MB (' + (size/1000).toFixed(2) + ' GB)';
             }
             else
             {
+                //formats multiple details with "," and "and"
                 details += listFormater(valInCat);
-            } //end else if more than one detail
+            }
         } //end else
 
+        //details is "" if there was a URL
         if(details != "")
         {
             newColumn.textContent = details;
         }
-    
+        
+        //appends new detail to table
         newTR.appendChild(newHeader);
         newTR.appendChild(newColumn);
         table.appendChild(newTR)
@@ -185,21 +215,25 @@ function getDescription()
   return table;
 }
 
+//icon to download model
 $("#downloadModel").click(function () {
     downloadModel();
-    console.log("download");
 });
 
+//download model using anchor tag
 function downloadModel()
 {
   var modelName = model["Name"];
 
+  //downloads model from svprojects
   var fileUrl = 'svprojects/' + modelName + '.zip';
   var a = document.createElement("a");
   a.href = fileUrl;
   a.setAttribute("download", modelName);
+  //simulates click to download
   a.click();
 
+  //messages server with download
   gtag('event', 'download_' + modelName, {
     'send_to': 'G-YVVR1546XJ',
     'event_category': 'Model download',
@@ -208,27 +242,34 @@ function downloadModel()
 });
 }
 
+//go to gallery icon
 $("#goToGallery").click(function () {
     goToGallery();
 });
 
+//brings user to dataset.html
 function goToGallery() {
+    //creates anchor tag and simulates click
     var a = document.createElement("a");
     a.href = "dataset.html";
     a.click();
 }
 
+//function to display multiple models in a table
 function displayTableModels(models)
 {
+    //creates "header"
     var div = document.getElementById("modelsTable");
     var h1 = document.createElement("h1");
-    h1.classList.add("titleForTableModels")
+    h1.classList.add("titleForTableModels");
     h1.textContent = "You are viewing " + models.length + " models.";
     div.appendChild(h1);
     
+    //sets up table
     var table = document.createElement("table");
     var categoryNames = getBareMinimum();
 
+    //appends all titles in one row
     var titleRow = document.createElement("tr");
     for(var c = 0; c < categoryNames.length; c++)
     {
@@ -238,47 +279,63 @@ function displayTableModels(models)
         titleRow.appendChild(newTitle);
     }
     table.appendChild(titleRow);
+
     var hooks = []
+    //cycles through all the models
     for(var m = 0; m < models.length; m++)
     {
+        //new row per model
         var modelRow = document.createElement("tr");
         modelRow.classList.add("modelRow");
+        //gives ID so the row is click-able
         modelRow.setAttribute("id", models[m]["Name"] + "_row");
 
         for(var c = 0; c < categoryNames.length; c++)
-        {   
+        {  
             var newDetail = document.createElement("td");
+
+            //string is the detail
             var string = models[m][categoryNames[c]];
+
+            //accounts for multiple details
             if(categoryNames[c] != "Name")
                 string = listFormater(string);
+            
             newDetail.textContent = string;
             
             if(categoryNames[c] == "Species" && string == "Animal")
             {
+                //more specific species
                 newDetail.textContent = models[m]["Animal"];
             }
-
+            //adds details to row
             modelRow.appendChild(newDetail);
         }
 
         table.appendChild(modelRow);
 
+        //saves model to later create hook
         hooks.push(models[m]);
     }
-
+    //adds table to multiple models page
     div.appendChild(table);
+
+    //creates loop for hooks outside of function to avoid bug
     for(var i = 0; i < hooks.length; i++)
     {
         createHook(hooks[i]);
     }
 }
 
+//creates hooks for the rows of the models in the table
 function createHook(model)
 {
+    //click on the row, goes to view the individual model
     $('#' + model["Name"] + "_row").click(function() {goToModel(model);});
 }
 
 function goToModel(model){
+    //creates shareable URL
     var array = makeshiftSelectedModels(data, model)
 
     //opens new window with encoded model
