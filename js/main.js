05d3a07371441a42fc48cb55b2d6846f45202e02
Carole <carole.darve@gmail.com>
2022-06-28 19:15:47 -0700

    minor organization

--- js/main.js
+++ js/main.js
@@ -584,90 +584,6 @@ function errorMessage(isOn, whichToDisplay)
   }
 }
 
-/*****************************************************
-  MixItUp - Define a single object literal
-  to contain all filter custom functionality
-*****************************************************/
-var buttonFilter = {
-    // Declare any variables we will need as properties of the object
-    $filters: null,
-    groups: [],
-    outputArray: [],
-    outputString: '',
-
-    // The "init" method will run on document ready and cache any jQuery objects we will need.
-    init: function(){
-      var self = this; // As a best practice, in each method we will asign "this" to the variable "self" so that it remains scope-agnostic. We will use it to refer to the parent "buttonFilter" object so that we can share methods and properties between all parts of the object.
-
-      self.$filters = $('.cd-main-content');
-      self.$container = $('.cd-gallery ul');
-
-      self.$filters.find('.cd-filters').each(function(){
-          var $this = $(this);
-
-        self.groups.push({
-            $inputs: $this.find('.filter'),
-            active: '',
-            tracker: false
-        });
-      });
-
-      self.bindHandlers();
-    },
-
-    // The "bindHandlers" method will listen for whenever a button is clicked.
-    bindHandlers: function(){
-      var self = this;
-
-      self.$filters.on('click', 'a', function(e){
-          self.parseFilters();
-      });
-      self.$filters.on('change', function(){
-        self.parseFilters();
-      });
-    },
-
-    parseFilters: function(){
-      var self = this;
-
-      // loop through each filter group and grap the active filter from each one.
-      for(var i = 0, group; group = self.groups[i]; i++){
-        group.active = [];
-        group.$inputs.each(function(){
-          var $this = $(this);
-          if($this.is('input[type="radio"]') || $this.is('input[type="checkbox"]')) {
-            if($this.is(':checked') ) {
-              group.active.push($this.attr('data-filter'));
-            }
-          } else if($this.is('select')){
-            group.active.push($this.val());
-          } else if( $this.find('.selected').length > 0 ) {
-            group.active.push($this.attr('data-filter'));
-          }
-        });
-      }
-      self.concatenate();
-    },
-
-    concatenate: function(){
-      var self = this;
-
-      self.outputString = ''; // Reset output string
-
-      for(var i = 0, group; group = self.groups[i]; i++){
-          self.outputString += group.active;
-      }
-
-      // If the output string is empty, show all rather than none:
-      !self.outputString.length && (self.outputString = 'all');
-
-      // Send the output string to MixItUp via the 'filter' method:
-    if(self.$container.mixItUp('isLoaded')){
-        self.$container.mixItUp('filter', self.outputString);
-    }
-    }
-}
-
 // HOOKS FOR MAIN
 
 //open/close lateral filter
@@ -721,3 +637,87 @@ $('#max-age').keydown(function (e) {
 //hooks that check width
 $(window).ready(checkWidth);
 $(window).resize(checkWidth);
+
+/*****************************************************
+  MixItUp - Define a single object literal
+  to contain all filter custom functionality
+*****************************************************/
+var buttonFilter = {
+  // Declare any variables we will need as properties of the object
+  $filters: null,
+  groups: [],
+  outputArray: [],
+  outputString: '',
+
+  // The "init" method will run on document ready and cache any jQuery objects we will need.
+  init: function(){
+    var self = this; // As a best practice, in each method we will asign "this" to the variable "self" so that it remains scope-agnostic. We will use it to refer to the parent "buttonFilter" object so that we can share methods and properties between all parts of the object.
+
+    self.$filters = $('.cd-main-content');
+    self.$container = $('.cd-gallery ul');
+
+    self.$filters.find('.cd-filters').each(function(){
+        var $this = $(this);
+
+      self.groups.push({
+          $inputs: $this.find('.filter'),
+          active: '',
+          tracker: false
+      });
+    });
+
+    self.bindHandlers();
+  },
+
+  // The "bindHandlers" method will listen for whenever a button is clicked.
+  bindHandlers: function(){
+    var self = this;
+
+    self.$filters.on('click', 'a', function(e){
+        self.parseFilters();
+    });
+    self.$filters.on('change', function(){
+      self.parseFilters();
+    });
+  },
+
+  parseFilters: function(){
+    var self = this;
+
+    // loop through each filter group and grap the active filter from each one.
+    for(var i = 0, group; group = self.groups[i]; i++){
+      group.active = [];
+      group.$inputs.each(function(){
+        var $this = $(this);
+        if($this.is('input[type="radio"]') || $this.is('input[type="checkbox"]')) {
+          if($this.is(':checked') ) {
+            group.active.push($this.attr('data-filter'));
+          }
+        } else if($this.is('select')){
+          group.active.push($this.val());
+        } else if( $this.find('.selected').length > 0 ) {
+          group.active.push($this.attr('data-filter'));
+        }
+      });
+    }
+    self.concatenate();
+  },
+
+  concatenate: function(){
+    var self = this;
+
+    self.outputString = ''; // Reset output string
+
+    for(var i = 0, group; group = self.groups[i]; i++){
+        self.outputString += group.active;
+    }
+
+    // If the output string is empty, show all rather than none:
+    !self.outputString.length && (self.outputString = 'all');
+
+    // Send the output string to MixItUp via the 'filter' method:
+  if(self.$container.mixItUp('isLoaded')){
+      self.$container.mixItUp('filter', self.outputString);
+  }
+  }
+}
