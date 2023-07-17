93626a6ac854feb893bd9b74805b9cd6023474d0
Luca Pegolotti <lpego@stanford.edu>
2023-07-17 15:54:00 -0700

    Change svresults.csv

--- vmr-clinical_data/VMR_Rename_ClinicalData.ps1
+++
@@ -1,4 +0,0 @@
-$folder = "c:\Users\19195\Documents\VMR_Project\vmr-data\vmr-clinical_data" #target folder containing files
-$csv = "c:\Users\19195\Documents\VMR_Project\vmr-data\vmr-clinical_data\VMR_Rename_ClinicalData.csv" #path to CSV file
-
-cd ($folder); import-csv ($csv)| foreach {rename-item -path $_.path -newname $_.newfilename}
