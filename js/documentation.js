29efa74d184c6901c2571e04eb4b8de095aa98fb
Carole <carole.darve@gmail.com>
2023-08-02 09:30:24 -0700

    finalize tutorial documentation

--- js/documentation.js
+++ js/documentation.js
@@ -1,47 +1,37 @@
-// The function actually applying the offset
-function offsetAnchor() {
-    if (location.hash.length !== 0) {
-      window.scrollTo(window.scrollX, window.scrollY - 100);
+var acc = document.getElementsByClassName("tOCDropDown");
+var i;
+
+//opens the panels corresponding to the drop down menu that was clicked
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("selected");
+    var number = this.id.substring(this.id.length - 3)
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = document.getElementById("panel" + number);
+
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
     }
-  }
-  
-  // Captures click events of all <a> elements with href starting with #
-  $(document).on('click', 'a[href^="#"]', function(event) {
-    // Click events are captured before hashchanges. Timeout
-    // causes offsetAnchor to be called after the page jump.
-    window.setTimeout(function() {
-      offsetAnchor();
-    }, 0);
   });
-  
-  // Set the offset when entering page with hash present in the url
-  window.setTimeout(offsetAnchor, 0);
+}
 
-  var acc = document.getElementsByClassName("tOCDropDown");
-  var i;
-  
-  //opens the panels corresponding to the drop down menu that was clicked
-  for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-      /* Toggle between adding and removing the "active" class,
-      to highlight the button that controls the panel */
-      this.classList.toggle("selected");
-      var number = this.id.substring(this.id.length - 3)
-  
-      /* Toggle between hiding and showing the active panel */
-      var panel = document.getElementById("panel" + number);
+$('#opening_filter').click(function() {
+  var a = document.createElement("a");
+  a.href = "#Opening_the_filter_menu";
+  a.click();
+  offsetAnchor();
+});
 
-      if (panel.style.display === "block") {
-        panel.style.display = "none";
-      } else {
-        panel.style.display = "block";
-      }
-    });
-  }
+$('#top').click(function() {
+  scrollToTop()
+});
 
-  $('#opening_filter').click(function() {
-    var a = document.createElement("a");
-    a.href = "#Opening_the_filter_menu";
-    a.click();
-    offsetAnchor();
-  });
+
+function scrollToTop() {
+  window.scrollTo({top: 0, behavior: 'smooth'});
+}
